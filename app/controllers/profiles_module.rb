815a5384fd114d3413293c989d625c2b6d466347
benoit5699 <benoit.schulman@gmail.com>
2015-11-27 16:43:17 +0100

    add_profile_to. improved to work without aftf <=> call <=> post

--- app/controllers/profiles_module.rb
+++ app/controllers/profiles_module.rb
@@ -11,56 +11,40 @@ module ProfilesModule
 	
 
 	def add_profile_to(target_user, profile)
-		source_user =  profile.owner
+		source_user =  profile.owner # source_user == PotentialUser
 		
-		
-		
-		#PotentialUser (source_user) s/u are transfered to target_user
+#=======> PotentialUser (source_user) s/u are transferred to target_user
 		user_actions_sources = UserAction.where(supportable: source_user) 	
 		user_actions_targets = UserAction.where(supportable: target_user)	
 		
-		user_actions_sources.each do |x|
-			
+		user_actions_sources.each do |ua|			
 			#if target_user had s/u source_user OR target_user was already s/u by someone who also s/u source_user (ie user_actions_targets rules!)
-			if x.creator == target_user || user_actions_targets.collect{|y| y.creator}.include?(x.creator)
-				x.destroy
-			else
-				x.update_attributes(supportable: target_user)
+			if ua.creator == target_user || user_actions_targets.collect{|y| y.creator}.include?(ua.creator)
+				ua.destroy
+			else 
+				ua.update_attributes(supportable: target_user)
 			end
 		end
-		#CALL where callable: PotentialUser
+		
+#=======> Call.where(callable: source_user)...
 		destroy_calls = Call.where(callable: source_user)
-		destroy_calls.each do |x| 
-			if x.creator == target_user
-				x.destroy
+		destroy_calls.each do |call| 
+			if call.creator == target_user
+				call.destroy
 			else
-				conversation = x.conversation
-				futur_calls = conversation.calls.where(callable: target_user)
-				case
-					when !target_user.can_post?(conversation) && !target_user.can_call?(conversation)
-						x.update_attributes(callable: target_user)
-					when !target_user.can_post?(conversation) && target_user.can_call?(conversation)
-						post = target_user.parent_call(conversation).child_post
-						x.supporters.each do |y|
-							post.supporters << y if !post.supporters.include?(y) && !post.unsupporters.include?(y)
-						end
-						x.unsupporters.each do |y|
-							post.unsupporters << y if !post.supporters.include?(y) && !post.unsupporters.include?(y)
-						end
-						x.destroy
-					when target_user.can_post?(conversation) 
-						call = target_user.parent_call(conversation)
-						x.supporters.each do |y|
-							call.supporters << y if !call.supporters.include?(y) && !call.unsupporters.include?(y) && call.callable != y 
-						end
-						x.unsupporters.each do |y|
-							call.unsupporters << y if !call.supporters.include?(y) && !call.unsupporters.include?(y) && call.callable != y 
-						end
-						x.destroy
+				if !call.conversation.calls.where(callable: target_user).any?
+					call.update_attributes(callable: target_user)
+				else
+					last_call = call.conversation.calls.where(callable: target_user).last
+					call.object_actions.each do |oa| 
+						oa.update_attributes(object: last_call) if ObjectAction.find_by(object: last_call, creator: oa.creator).nil?
+					end
+					call.destroy
 				end
 			end
 		end
-		target_user.add_profile(profile)
+
+		profile.update_attributes(owner: target_user)
 		source_user.destroy! if source_user.class.name == "PotentialUser"
 	end
 end
