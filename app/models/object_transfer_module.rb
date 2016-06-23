64af22a649d038c34893eacc511342e19518008c
bebeun <benoit.schulman@gmail.com>
2016-06-23 02:19:47 +0200

    s/u transfer from call to post. s/u of initial calls are erased

--- app/models/object_transfer_module.rb
+++ app/models/object_transfer_module.rb
@@ -1,6 +1,7 @@
 module ObjectTransferModule
 	def transfer_up(call)
-		call.supporters.each{|y| self.supporters << y } and call.unsupporters.each{|y| self.unsupporters << y }
+		#call.supporters.each{|y| self.supporters << y } and call.unsupporters.each{|y| self.unsupporters << y }
+		call.object_actions.each{|oa|  oa.update_attributes(object: self) }
 	end
 end
 
